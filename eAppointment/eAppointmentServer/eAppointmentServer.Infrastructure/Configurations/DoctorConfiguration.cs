017f89b7fdfac1997f8387e0637de84a058fabc4
Bahadir Aygul <bhdrygl8@gmail.com>
2025-06-03 12:32:40 +0300

    add Inftastructure layer

---
+++ eAppointment/eAppointmentServer/eAppointmentServer.Infrastructure/Configurations/DoctorConfiguration.cs
@@ -0,0 +1,18 @@
+﻿
+
+using eAppointmentServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eAppointmentServer.Infrastructure.Configurations;
+
+internal sealed class DoctorConfiguration
+{
+    public void Configure(EntityTypeBuilder<Doctor> builder)
+    {
+        builder.Property(p => p.FirstName).HasColumnType("varChar(50)");
+        builder.Property(p => p.LastName).HasColumnType("varChar(50)");
+
+        // Additional configurations can be added here
+    }
+}
